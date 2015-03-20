b990d6aa913f1c2dbcb95e540a2c13e4afc139fd
QiuQiu <ricklovelisa@126.com>
2015-03-20 16:33:18 +0800

    xiugailedaima

--- chi_test.R
+++ chi_test.R
@@ -4,19 +4,10 @@ library(RTextTools)
 
 test.train <- readRDS("test_train.rds")
 
-train.title <- strsplit(test.train$title, split = "")
-names(train.title) <- test.train$ID
-train.content <- strsplit(test.train$content, split = "")
-names(train.content) <- test.train$ID
+train.title <- readRDS("train_title.rds")
+train.content <-readRDS("train_content.rds")
 
-train.title <- sapply(train.title, function(x) gsub("[a-zA-Z]", "", removePunctuation(removeNumbers(x))))
-train.title <- sapply(train.title, function(x) gsub("\\s", "", x))
-train.title <- sapply(train.title, function(x) x[nchar(x) != 0])
 train.title <- sapply(train.title, function(x) list(list(x)))
-
-train.content <- sapply(train.content, function(x) gsub("[a-zA-Z]", "", removePunctuation(removeNumbers(x))))
-train.content <- sapply(train.content, function(x) gsub("\\s", "", x))
-train.content <- sapply(train.content, function(x) x[nchar(x) != 0])
 train.content <- sapply(train.content, function(x) list(list(x)))
 
 corpus.train.title <- Corpus(VectorSource(train.title), readerControl = list(language = "ZHCN"))
@@ -25,19 +16,19 @@ for (i in 1:length(corpus.train.title)){
   corpus.train.title[[i]]$content <- sub("c", "", corpus.train.title[[i]]$content)
 }
 for (i in 1:length(corpus.train.title)){
-  meta(corpus.train.title[[i]], tag = 'id') <- test.train$ArticleId[i]
+  meta(corpus.train.title[[i]], tag = 'id') <- test.train$ID[i]
 }
 
 for (i in 1:length(corpus.train.content)){
   corpus.train.content[[i]]$content <- sub("c", "", corpus.train.content[[i]]$content)
 }
 for (i in 1:length(corpus.train.content)){
-  meta(corpus.train.content[[i]], tag = 'id') <- test.train$ArticleId[i]
+  meta(corpus.train.content[[i]], tag = 'id') <- test.train$ID[i]
 }
 
 
-control.tfidf <- list(removePunctuation = F, removeNumbers = T, stripWhitespace = T, wordLengths = c(2, 10), weighting = function(x)weightTfIdf(x, normalize = F))
-control.tf <- list(removePunctuation = F, removeNumbers = T, stripWhitespace = T, wordLengths = c(2, 10))
+control.tfidf <- list(removePunctuation = T, removeNumbers = T, stripWhitespace = T, wordLengths = c(1, 10), weighting = function(x)weightTfIdf(x, normalize = F))
+control.tf <- list(removePunctuation = F, removeNumbers = T, stripWhitespace = T, wordLengths = c(1, 10))
 
 dtm.tfidf.train.title <- DocumentTermMatrix(corpus.train.title, control.tfidf)
 dtm.tfidf.train.content <- DocumentTermMatrix(corpus.train.content, control.tfidf)
