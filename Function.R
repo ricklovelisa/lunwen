443aa05bf2e67e289ce33a514f5a202bc980818f
QQ <qiuqiu@localhost.(none)>
2015-03-23 10:04:17 +0800

    抽取方法，修改代码

---
+++ Function.R
@@ -0,0 +1,14 @@
+MakePredDtm <- function(dtm,weighting = "tf"){
+  terms <- colnames(dtm[,which(!colnames(dtm) %in% colnames(pred))])
+  weight <- 0
+  amat <- matrix(weight,nrow=nrow(pred),ncol=length(terms))
+  colnames(amat) <- terms
+  rownames(amat) <- rownames(pred)
+  if(weighting == "tf"){
+    fixed <- as.DocumentTermMatrix(cbind(pred[,which(colnames(pred) %in% colnames(dtm))],amat),weighting=weightTf)
+  }else if(weighting == "tfidf"){
+    fixed <- as.DocumentTermMatrix(cbind(pred[,which(colnames(pred) %in% colnames(dtm))],amat),weighting=weightTfidf)
+  }
+  pred <- fixed
+  return(pred)
+}
