977df048b7132ef30d3390a4c62b6188c470b34a
QQ <qiuqiu@localhost.(none)>
2015-03-23 10:15:45 +0800

    分离代码，整理格式

---
+++ Prediction.R
@@ -0,0 +1,13 @@
+preddata <- c("")
+preddata <- strsplit(preddata, split = "")
+preddata <- sapply(preddata, function(x) list(list(x)))
+pred <- Corpus(VectorSource(preddata), readerControl = list(language = "ZHCN"))
+for (i in 1:length(pred)){
+  pred[[i]]$content <- sub("c", "", pred[[i]]$content)
+}
+for (i in 1:length(pred)){
+  meta(pred[[i]], tag = 'id') <- test.train$ID[i]
+}
+pred <- DocumentTermMatrix(pred, tf)
+pred <- MakePredDtm(pred, "tf")
+predict(SVM, pred)
--- SVM.R
+++ SVM.R
@@ -40,24 +40,3 @@ dtm <- dtm[, -c(1:25)]
 
 SVM <- svm(dtm, as.factor(test.train$source), type = "C-classification", kernel = "radial")
 
-preddata <- c("")
-preddata <- strsplit(preddata, split = "")
-preddata <- sapply(preddata, function(x) list(list(x)))
-
-pred <- Corpus(VectorSource(preddata), readerControl = list(language = "ZHCN"))
-for (i in 1:length(pred)){
-  pred[[i]]$content <- sub("c", "", pred[[i]]$content)
-}
-for (i in 1:length(pred)){
-  meta(pred[[i]], tag = 'id') <- test.train$ID[i]
-}
-
-
-pred <- DocumentTermMatrix(pred, tf)
-
-pred <- MakePredDtm(pred, "tf")
-
-
-
-predict(SVM, pred)
-pred
