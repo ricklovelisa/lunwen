1335982b99801528f43613c8ed210f6d33a01872
QQ <qiuqiu@localhost.(none)>
2015-03-24 11:36:54 +0800

    修复了几个bug

--- Function.R
+++ Function.R
@@ -1,14 +1,15 @@
-MakePredDtm <- function(pred,dtm,weighting = "tf"){
+MakePredDtm <- function(pred, dtm){ # weighting = "tf" 暂时不要用tfidf
   terms <- colnames(dtm[,which(!colnames(dtm) %in% colnames(pred))])
-  weight <- 0
-  amat <- matrix(weight,nrow=nrow(pred),ncol=length(terms))
+  amat <- matrix(0, nrow = nrow(pred), ncol = length(terms))
   colnames(amat) <- terms
   rownames(amat) <- rownames(pred)
-  if(weighting == "tf"){
-    fixed <- as.DocumentTermMatrix(cbind(pred[,which(colnames(pred) %in% colnames(dtm))],amat),weighting=weightTf)
-  }else if(weighting == "tfidf"){
-    fixed <- as.DocumentTermMatrix(cbind(pred[,which(colnames(pred) %in% colnames(dtm))],amat),weighting=weightTfidf)
-  }
+#   if(weighting == "tf"){
+  fixed <- as.DocumentTermMatrix(cbind(pred[,which(colnames(pred) %in% colnames(dtm))],amat),weighting=weightTf)
+#   }
+# else if(weighting == "tfidf"){
+# fixed <- as.DocumentTermMatrix(cbind(pred[,which(colnames(pred) %in% colnames(dtm))],amat),weighting=weightTf)
+# fixed[, pred[,which(colnames(pred) %in% colnames(dtm))]] <- 
+# }
   pred <- fixed
   return(pred)
 }
