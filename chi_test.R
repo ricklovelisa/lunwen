70c6d5a00bd5a81e40549fbdf3727bc1e52e7ecd
QiuQiu <ricklovelisa@126.com>
2015-03-26 17:06:26 +0800

    修改卡方检验代码

--- chi_test.R
+++ chi_test.R
@@ -3,20 +3,22 @@ library(slam)
 library(e1071)
 library(jiebaR)
 
-text <- list("I love you","I hate you")
-corpus <- Corpus(VectorSource(text))
-dtm <- DocumentTermMatrix(corpus)
-cate <- c("P","N")
-# word and category are related
+test.train <- readRDS("Data_&_Model/test_train.rds")
+dtm <- readRDS("Data_&_Model/dtm_tf_content_seg_noclean.rds")
 
-temp <- as.matrix(dtm[,1])
-temp[]
-a <- c(1,0,1,0,0,0,1)
-b <- c("a", "b","a","a","b","a","b")
-for(i in 1:length(Terms(dtm))){
-  terms <- as.matrix(dtm[,i])
-  terms[terms != 0] <- 1
-  XsqMatrix[,i] <- table(terms,cate)
-  chisq.test()
+
+
+# chisq test #
+chisq <- matrix(0, nrow = length(Terms(dtm)), ncol = 10)
+for(i in 1:10){
+  cate <- test.train$source
+  cate <- ifelse(cate == unique(cate)[i], unique(cate)[i], "other")
+  for(j in 1:length(Terms(dtm))){
+    terms <- as.matrix(dtm[,j])
+    terms[terms != 0] <- 1
+    XsqMatrix <- table(terms,cate) # confusion matrix
+    chisq[j, i] <- chisq.test(XsqMatrix, correct = F)$statistic
+    cat("======", j, "======\n" )
+  }
 }
 
