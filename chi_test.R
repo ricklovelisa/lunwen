db0685090595e94a2bb4a6d22baf98909d734002
Qiu Qiu <ricklovelisa@126.com>
2015-03-26 23:30:55 +0800

    修正卡方检验代码

--- chi_test.R
+++ chi_test.R
@@ -10,14 +10,13 @@ dtm <- dtm[row_sums(dtm) > 0, ]
 
 
 term_tfidf <- tapply(dtm$v/row_sums(dtm)[dtm$i], dtm$j, mean) * log2(nDocs(dtm)/col_sums(dtm > 0))
-dtm2 <- dtm[,term_tfidf >= 0.2]
-dtm2 <- dtm[row_sums(dtm2) > 0, ]
-
+dtm <- dtm[,term_tfidf >= 0.1]
+dtm <- dtm[row_sums(dtm) > 0, ]
 
 # chisq test #
 CATE <- as.vector(sapply(rownames(dtm), function(x) strsplit(x,split = "_")[[1]][2]))
-chisq <- matrix(0, nrow = length(Terms(dtm)), ncol = 10)
-for(i in 1:10){
+chisq <- matrix(0, nrow = length(Terms(dtm)), ncol = length(unique(CATE)))
+for(i in 1:length(unique(CATE))){
   cate <- CATE
   cate <- ifelse(cate == unique(cate)[i], unique(cate)[i], "other")
   for(j in 1:length(Terms(dtm))){
@@ -25,7 +24,7 @@ for(i in 1:10){
     terms[terms != 0] <- 1
     XsqMatrix <- table(terms,cate) # confusion matrix
     chisq[j, i] <- chisq.test(XsqMatrix, correct = F)$statistic
-    cat("======", j, "======\n" )
   }
+  cat("======", i, "======\n" )
 }
 
