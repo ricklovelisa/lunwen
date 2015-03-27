1307bfd30945c531b66726e876be2cf3f66138af
QQ <qiuqiu@localhost.(none)>
2015-03-27 16:49:22 +0800

    抽象出了一些function

--- Classification/Function.R
+++ Classification/Function.R
@@ -1,5 +1,4 @@
-setwd("Classification/")
-
+# 将测试集和训练集统一 #
 MakePredDtm <- function(pred, dtm){ # weighting = "tf" 暂时不要用tfidf
   terms <- colnames(dtm[,which(!colnames(dtm) %in% colnames(pred))])
   amat <- matrix(0, nrow = nrow(pred), ncol = length(terms))
@@ -16,8 +15,28 @@ MakePredDtm <- function(pred, dtm){ # weighting = "tf" 暂时不要用tfidf
   return(pred)
 }
 
+
+# 计算df #
 DocFreq <- function(dtm){
   dtm$v[dtm$v != 1] <- 1
   df <- col_sums(dtm)
   return(df)
 }
+
+# 支持dtm的卡方检验 #
+ChisqareTest <- function(dtm, label){
+  chisq <- matrix(0, nrow = length(Terms(dtm)), ncol = length(unique(label)))
+  for(i in 1:length(unique(label))){
+    cate <- label
+    cate <- ifelse(cate == unique(cate)[i], unique(cate)[i], "other")
+    for(j in 1:length(Terms(dtm))){
+      terms <- as.matrix(dtm[,j])
+      terms[terms != 0] <- 1
+      # terms[terms == 0] <- 2
+      XsqMatrix <- table(terms,cate) # confusion matrix
+      chisq[j, i] <- chisq.test(XsqMatrix, correct = F)$statistic
+    }
+  }
+  return(chisq)
+}
+
--- Classification/chi_test.R
+++ Classification/chi_test.R
@@ -8,7 +8,8 @@ library(jiebaR)
 
 test.train <- readRDS("Data_&_Model/test_train.rds")
 dtm <- readRDS("Data_&_Model/dtm_tf_content_seg_noclean.rds")
-df <- 
+df <- DocFreq(dtm)
+dtm <- dtm[, df >= 2]
 
 # dtm <- dtm[,-c(1:26872)]
 # dtm <- dtm[row_sums(dtm) > 0, ]
@@ -20,16 +21,7 @@ df <-
 
 # chisq test #
 category <- as.vector(sapply(rownames(dtm), function(x) strsplit(x,split = "_")[[1]][2]))
-chisq <- matrix(0, nrow = length(Terms(dtm)), ncol = length(unique(CATE)))
-for(i in 1:length(unique(CATE))){
-  cate <- category
-  cate <- ifelse(cate == unique(cate)[i], unique(cate)[i], "other")
-  for(j in 1:length(Terms(dtm))){
-    terms <- as.matrix(dtm[,j])
-    terms[terms != 0] <- 1
-    # terms[terms == 0] <- 2
-    XsqMatrix <- table(terms,cate) # confusion matrix
-    chisq[j, i] <- chisq.test(XsqMatrix, correct = F)$statistic
-  }
-  cat("======", i, "======\n" )
-}
+chisq <- ChisqareTest(dtm, category)
+
+
+
