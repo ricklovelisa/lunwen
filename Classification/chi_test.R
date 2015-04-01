76143a2277adaab457d8efa4d3a6199737d1d180
Qiu Qiu <ricklovelisa@126.com>
2015-04-01 12:51:11 +0800

    修改了代码

--- Classification/chi_test.R
+++ Classification/chi_test.R
@@ -12,14 +12,15 @@ df <- DocFreq(dtm)
 dtm <- dtm[, df >= 2]
 dtm <- dtm[, -c(1:11918)]
 dtm <- dtm[row_sums(dtm) > 0, ]
+term_tfidf <- tapply(dtm$v/row_sums(dtm)[dtm$i], dtm$j, mean) * log2(nDocs(dtm)/col_sums(dtm > 0))
+dtm <- dtm[,term_tfidf >= 0.1]
+dtm <- dtm[row_sums(dtm) > 0, ]
 
 # dtm <- dtm[,-c(1:26872)]
 # dtm <- dtm[row_sums(dtm) > 0, ]
 
 
-# term_tfidf <- tapply(dtm$v/row_sums(dtm)[dtm$i], dtm$j, mean) * log2(nDocs(dtm)/col_sums(dtm > 0))
-# dtm <- dtm[,term_tfidf >= 0.1]
-# dtm <- dtm[row_sums(dtm) > 0, ]
+
 
 # chisq test #
 category <- as.vector(sapply(rownames(dtm), function(x) strsplit(x,split = "_")[[1]][2]))
